0b29cdeff5fbc0e33be4cbd0b8bbe10604ee7379
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-11-18 13:15:17 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,47 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Задание №1
+/*        Scanner s = new Scanner(System.in);
+        System.out.println("Введите число: ");
+
+        if (s.nextInt() > 7) {
+            System.out.println("Привет");
+        }
+        s.close();*/
+
+
+        //Задание №2
+/*        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя: ");
+        if(Objects.equals(scanner.nextLine(), "Вячеслав")){
+            System.out.println("Привет, Вячеслав");
+        }else{
+            System.out.println("Нет такого имени");
+        }
+        scanner.close();*/
+
+
+        //Задание №3
+/*
+        int[] a = new int[50];
+        for(int i = 0; i < a.length; i++) {
+            a[i] = i + 1;
+            if (a[i] % 3 == 0){
+                System.out.println(a[i]);
+            }
+        }*/
+
+
+        //Задание №4
+        /*
+        Дана скобочная последовательность: [((())()(())]]
+        Она неверная, вот правильная скобочная последовательность [[((()))()(())]]
+         */
+    }
+
+
+}
