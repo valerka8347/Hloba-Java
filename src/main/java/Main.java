c98eb7c44eb0d0ef2106bdae5bb5510d0ed0328e
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-11-21 11:46:56 +0300

    new

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,35 +5,33 @@ public class Main {
     public static void main(String[] args) {
 
         //Задание №1
-/*        Scanner s = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         System.out.println("Введите число: ");
 
         if (s.nextInt() > 7) {
             System.out.println("Привет");
         }
-        s.close();*/
+
 
 
         //Задание №2
-/*        Scanner scanner = new Scanner(System.in);
+       /* Scanner sс = new Scanner(System.in);
         System.out.println("Введите имя: ");
-        if(Objects.equals(scanner.nextLine(), "Вячеслав")){
+        if(Objects.equals(sс.nextLine(), "Вячеслав")){
             System.out.println("Привет, Вячеслав");
         }else{
             System.out.println("Нет такого имени");
-        }
-        scanner.close();*/
+        }*/
 
 
         //Задание №3
-/*
         int[] a = new int[50];
         for(int i = 0; i < a.length; i++) {
             a[i] = i + 1;
             if (a[i] % 3 == 0){
-                System.out.println(a[i]);
+                System.out.print(a[i]);
             }
-        }*/
+        }
 
 
         //Задание №4
